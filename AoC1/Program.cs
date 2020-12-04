9e017eab73d5e3e5d7304691d0e9d4e55c9828db
Mikkel <mikkel_hansen_1998@hotmail.com>
2020-12-04 22:54:21 +0100

    Add project files.

---
+++ AoC1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AoC_Utilities;
+
+namespace AoC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                string[] input = AocInputReader.GetChallengeInput(i, true);
+
+                Console.WriteLine("From Challenge " + i + ":");
+                for (int j = 0; j < input.Length; j++)
+                {
+                    Console.WriteLine(string.Format("{0}: {1}", j, input[j]));
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AoC_Utilities/AocInputReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace AoC_Utilities
+{
+    public static class AocInputReader
+    {
+        public const string CHALLENGE_FOLDER_PATH = @"C:\Users\Mikkel\source\repos\AoC2020\AoC Input\";
+
+        public static string GetChallengeInputName(int challengeNumber)
+        {
+            return CHALLENGE_FOLDER_PATH + challengeNumber.ToString() + ".txt";
+        }
+
+        public static string[] GetChallengeInput(int challengeNumber, bool lines = true)
+        {
+            string challengePath = GetChallengeInputName(challengeNumber);
+
+            if (File.Exists(challengePath))
+            {
+                StreamReader sr = File.OpenText(challengePath);
+
+                if (lines)
+                {
+                    List<string> input = new List<string>();
+                    string nextInput;
+
+                    while ((nextInput = sr.ReadLine()) != null)
+                    {
+                        input.Add(nextInput);
+                    }
+
+                    return input.ToArray();
+
+                } else
+                {
+                    return new string[] { sr.ReadToEnd() };
+                }
+
+                sr.Dispose();
+
+            } else Console.WriteLine("Challenge input does not exist!");
+
+            return new string[] { };
+        }
+    }
+}
